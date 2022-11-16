a946834e56421c80bbee49a5c745d617aa5c15d2
Mercy Nyong <nyongmercy@gmail.com>
2022-11-16 03:02:23 +0100

    added javascript file

--- js/main.js
+++ js/main.js
@@ -1,59 +1,28 @@
-const slides = document.querySelector(".slides");
-const slide = Array.from(slides.children);
-const next = document.querySelector(".next");
-const prev = document.querySelector(".prev");
-const dots = document.querySelector(".dots");
-const dot = Array.from(dots.children);
+let slideIndex = 1;
+showSlides(slideIndex);
 
-const slideWidth = slide[0].getBoundingClientRect().width;
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
 
-const setSlidePosition = (slide, index) => {
-	slide.style.left = index * slideWidth + "px";
-};
-slide.forEach(setSlidePosition);
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
 
-const moveToSlide = (slides, currentSlide, targetSlide) => {
-	slides.style.transform = "translateX(-" + targetSlide.style.left + ")";
-	currentSlide.classList.remove("current-slide");
-	targetSlide.classList.add("current-slide");
-};
-
-const updateDots = (currentDot, targetDot) => {
-	currentDot.classList.remove("current-slide");
-	targetDot.classList.add("current-slide");
-};
-
-prev.addEventListener("click", (e) => {
-	const currentSlide = slides.querySelector(".current-slide");
-	const prevSlide = currentSlide.previousElementSibling;
-	const currentDot = dots.querySelector(".current-slide");
-	const prevDot = currentDot.previousElementSibling;
-
-	moveToSlide(slides, currentSlide, prevSlide);
-	updateDots(currentDot, prevDot);
-});
-
-next.addEventListener("click", (e) => {
-	const currentSlide = slides.querySelector(".current-slide");
-	const nextSlide = currentSlide.nextElementSibling;
-	const currentDot = dots.querySelector(".current-slide");
-	const nextDot = currentDot.nextElementSibling;
-
-	moveToSlide(slides, currentSlide, nextSlide);
-	updateDots(currentDot, nextDot);
-});
-
-dots.addEventListener("click", (e) => {
-	const targetDot = e.target.closest(".dot");
-
-	if (!targetDot) {
-		return;
-	}
-
-	const currentSlide = slides.querySelector(".current-slide");
-	const currentDot = dots.querySelector(".current-slide");
-	const targetIndex = dot.findIndex((dot) => dot === targetDot);
-	const targetSlide = slide[targetIndex];
-	moveToSlide(slides, currentSlide, targetSlide);
-	updateDots(currentDot, targetDot);
-});
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
